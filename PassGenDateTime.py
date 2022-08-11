3611752adcf94a36b4c2f482a8a66e95a3ee420e
RomanMishel <107872462+RomanMishel@users.noreply.github.com>
2022-08-11 19:25:48 +0300

    init

--- PassGenDateTime.py
+++ PassGenDateTime.py
@@ -5,13 +5,17 @@ current_time = datetime.datetime.today()
 
 day = current_time.day
 mount = current_time.month
-
+max_mounts = 12
+max_days_of_the_mount = 31
 print(day, mount)
 def reminder():
-    user_inpur_date1 = int(input('Choose the mount to change password: '))
-    user_inpur_date2 = int(input('Choose the day of the mount to change password: '))
-        if user_inpur_date1 > 12:
-            print('Wrong mount number\nChoose between 1 and 12')
+    user_input_date1 = int(input('Choose the mount to change password: '))
+    user_input_date2 = int(input('Choose the day of the mount to change password: '))
+    if user_input_date1 > max_mounts:
+        print('Wrong mount number\nChoose between 1 and 12!')
+    if user_input_date2 > max_days_of_the_mount:
+        print('Wrong date\nChoose between 1 and 31!')
+
 
 
 
