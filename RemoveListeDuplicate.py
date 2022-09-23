1371359246f4b68cb8296d7d1a37ec43ecb3d309
RomanMishel <107872462+RomanMishel@users.noreply.github.com>
2022-09-23 20:53:58 +0300

    init

---
+++ RemoveListeDuplicate.py
@@ -0,0 +1,17 @@
+def rld():
+    name_list = ["Michelle", "Sarah", "Roman", "Micheal", "Sarah", "Michelle"]
+    new_list = []
+
+    for x in name_list:
+        print(new_list)
+
+        if x not in new_list:
+            new_list.append(x)
+
+        else:
+            return new_list
+
+
+rld()
+
+
