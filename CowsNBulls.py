81ddfbfddacdda42c4d4cdae87d3e1b408fe4f05
RomanMishel <107872462+RomanMishel@users.noreply.github.com>
2022-10-14 19:50:07 +0300

    init

--- CowsNBulls.py
+++ CowsNBulls.py
@@ -1,10 +1,13 @@
 import random
 
-rand_new = random.randrange(0,10000,1)
+rand_new = random.randrange(0,10000)
 ran_list = [rand_new]
 
-def cows_n_bulls
+def cows_n_bulls():
 
     user_input = input('Guess a number: ')
+        for i in range(len(ran_list)):
+
+
 
 
